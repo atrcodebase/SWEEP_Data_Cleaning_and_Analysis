d25f5287856e7a7ff36fde4219dd59b516135cad
atrcodebase <datamanagement.atr@gmail.com>
2022-02-22 11:33:09 +0430

    updated the script to consider only shared cases while comparing callback vs. actual data

--- R/5- backcheck tables.R
+++ R/5- backcheck tables.R
@@ -14,13 +14,17 @@ backcheck_analysis <- plyr::join(backcheck_analysis,
 
 # comparison
 backcheck_casids <- unique(backcheck_complete$caseid)
+df_complete_approved_caseids <- unique(df_complete_approved$caseid)
+shared_caseids <- backcheck_casids[backcheck_casids %in% df_complete_approved_caseids]
+
 comparison <- rbind(
   backcheck_complete %>% 
+    filter(caseid %in% shared_caseids) %>% 
     select(caseid, m2a, m2b, m2c, m2d, m2j, m2pboy, m2p, mob7, Q10cbsg5, KEY) %>% 
     mutate(source = "backcheck")
   ,
   df_complete_approved %>% 
-    filter(caseid %in% backcheck_casids) %>% 
+    filter(caseid %in% shared_caseids) %>% 
     select(caseid, m2a, m2b, m2c, m2d, m2j, m2pboy, m2p, mob7, Q10cbsg5, KEY) %>% 
     mutate(source = "actual_data")
 )
