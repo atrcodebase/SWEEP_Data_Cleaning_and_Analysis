b0262024b6da952802ac4c8e1b4d5adc2caef6f7
atrcodebase <datamanagement.atr@gmail.com>
2022-02-23 14:56:44 +0430

    updated the "remove_columns" vector to remove extra columns

--- R/1- data cleaning.R
+++ R/1- data cleaning.R
@@ -1,32 +1,32 @@
 df <- read_excel(data_path, guess_max = 10000)
-remove_columns <- c("subscriberid", "simid", "devicephonenum", "instance_time", "text_audit",
-                    "phone_call_log", "phone_call_duration", "collect_phone_app", "call_time",
-                    "reschedule", "reschedule_full", "reschedule_no_ans",  "call_respondent",
-                    "device_info", "address", "users", "pub_to_users", "call_datetime", 
-                    "new_sortby", "num_calls", "Please_Select_The_Qa_Status_Of_The_Survey",
-                    "needs_review", "userid", "AA1",
-                    "AA2", "AA3", "AA4", "AA5", "end_time",
-                    "qa", "instanceID", "instanceName", "formdef_version", "review_quality",
-                    "review_comments", "review_corrections",
-                    "Surveyor_Name_label", "District_New_label", "Province_New_label", 
-                    "deviceid", "callback_time", "last_call_status", "call_num", "stop_at", "now_complete", "full_name", "call_date", "Surveyor_Name", "phone_response", "Reached_out_to_respondent",	"Not_reached_out_to_respondent",	"Is_the_target_respondent_living_in_this_hh", "Phone_passed_over", "Obtions_to_reach_out_to_respondent", "Obtions_to_reach_out_to_respondent_label", "callback_availability",
-                    "wfws12_Own", "wfws12_place_", "wfws12_at", "wfws12_home"
+remove_columns <- c("subscriberid", "simid", "devicephonenum", "username", "instance_time", "text_audit", "phone_call_log", "phone_call_duration",
+                    "collect_phone_app", "call_time", "reschedule", "reschedule_full", "reschedule_no_ans",  "call_respondent", "device_info",
+                    "address", "users", "pub_to_users", "call_datetime", "new_sortby", "num_calls", "Please_Select_The_Qa_Status_Of_The_Survey",
+                    "needs_review", "userid", "AA1", "AA2", "AA3", "AA4", "AA5", "end_time", "qa", "instanceID", "instanceName", "formdef_version",
+                    "review_quality", "review_comments", "review_corrections", "Surveyor_Name_label", "District_New_label", "Province_New_label",
+                    "deviceid", "callback_time", "last_call_status", "call_num", "stop_at", "now_complete", "full_name", "call_date", "Surveyor_Name",
+                    "phone_response", "phone_response_label", "Reached_out_to_respondent", "Reached_out_to_respondent_label",	"Not_reached_out_to_respondent",
+                    "Not_reached_out_to_respondent_label",	"Is_the_target_respondent_living_in_this_hh", "Is_the_target_respondent_living_in_this_hh_label",
+                    "Phone_passed_over", "Phone_passed_over_label", "Obtions_to_reach_out_to_respondent", "Obtions_to_reach_out_to_respondent_label",
+                    "callback_availability", "callback_availability_label", "wfws12_Own", "wfws12_place_", "wfws12_at", "wfws12_home", "resp_pn1",
+                    "resp_pn2", "resp_pn3", "resp_pn4", "resp_pn5", "calltime", "sfcbsg1", "resp_pn", "alternate_resp_pn", "answered_response",
+                    "answered_response_label", "whynot", "whynot_label", "whynot_other", "resp_pn0",	"resp_pn0_label", "AA_Full"
                     )
 
 if (sum(!remove_columns %in% colnames(df)) > 0 ) {
   remove_columns <- remove_columns[remove_columns %in% colnames(df)]
 }
 
-df <- select(df, -remove_columns)
+df <- select(df, -all_of(remove_columns))
 df <- select(df, -c(grep("_label", colnames(df))-1))
 colnames(df) <- str_remove(colnames(df), "_label")
 df <- df %>% 
-  mutate(
-    AA_Full = gsub("File skipped from exports: |File skipped from exports: media/", "", AA_Full),
-    AA_Full = case_when(
-      !is.na(AA_Full) ~ glue::glue("https://atrconsultingaf.surveycto.com/view/submission-attachment/{AA_Full}?uuid=uuid%3A{str_remove(KEY, 'uuid:')}"),
-      TRUE ~ AA_Full
-    )) %>% 
+  # mutate(
+  #   AA_Full = gsub("File skipped from exports: |File skipped from exports: media/", "", AA_Full),
+  #   AA_Full = case_when(
+  #     !is.na(AA_Full) ~ glue::glue("https://atrconsultingaf.surveycto.com/view/submission-attachment/{AA_Full}?uuid=uuid%3A{str_remove(KEY, 'uuid:')}"),
+  #     TRUE ~ AA_Full
+  #   )) %>% 
   mutate(
     Province = toupper(Province),
     District = toupper(str_remove(District, "_bad|_bam|_par|_tak"))
--- R/4- backcheck.R
+++ R/4- backcheck.R
@@ -1,33 +1,30 @@
 backcheck <- readxl::read_excel(backcheck_path)
-remove_columns <- c("deviceid", "subscriberid", "simid", "devicephonenum", "username", "instance_time", "text_audit",
-                    "phone_call_log", "phone_call_duration", "collect_phone_app", "device_info", "full_name", "users",
-                    "pub_to_users", "num_calls", "call_num", "stop_at", "now_complete", "userid", "calltime", "Supervisor_Name",
-                    "Supervisor_Name_label", "Call_Type", "Call_Type_label", "call_respondent", "phone_response",
-                    "phone_response_label", "Not_reached_out_to_respondent", "Not_reached_out_to_respondent_label",
+remove_columns <- c("deviceid", "subscriberid", "simid", "devicephonenum", "username", "instance_time", "text_audit", "phone_call_log",
+                    "collect_phone_app", "device_info", "full_name", "users", "pub_to_users", "num_calls", "call_num", "stop_at", "now_complete",
+                    "userid", "calltime", "Supervisor_Name", "Supervisor_Name_label", "Call_Type", "Call_Type_label", "call_respondent",
+                    "phone_response", "phone_response_label", "Not_reached_out_to_respondent", "Not_reached_out_to_respondent_label",
                     "Is_the_target_respondent_living_in_this_hh", "Is_the_target_respondent_living_in_this_hh_label", "Phone_passed_over",
                     "Phone_passed_over_label", "Phone_passed_over_other", "answered_response", "answered_response_label", "reschedule",
-                    "instanceID", "instanceName", "formdef_version"
+                    "instanceID", "instanceName", "formdef_version", "resp_pn", "Surveyor_Gender", "Surveyor_ID", "resp_ver", "resp_ver_label",
+                    "respondent_gender", "respondent_gender_label", "respondent_cbsg_member", "respondent_cbsg_member_label", "AA_Full"
                     )
 
 if (sum(!remove_columns %in% colnames(backcheck)) > 0 ) {
   remove_columns <- remove_columns[remove_columns %in% colnames(backcheck)]
 }
 
-backcheck <- select(backcheck, -remove_columns)
+backcheck <- select(backcheck, -all_of(remove_columns))
 backcheck <- select(backcheck, -c(grep("_label", colnames(backcheck))-1))
 colnames(backcheck) <- str_remove(colnames(backcheck), "_label")
 
-backcheck <- backcheck %>% 
-  mutate(
-    AA_Full = gsub("File skipped from exports: |File skipped from exports: media/", "", AA_Full),
-    AA_Full = case_when(
-      !is.na(AA_Full) ~ glue::glue("https://atrconsultingaf.surveycto.com/view/submission-attachment/{AA_Full}?uuid=uuid%3A{str_remove(KEY, 'uuid:')}"),
-      TRUE ~ AA_Full
-    ))
+# backcheck <- backcheck %>% 
+#   mutate(
+#     AA_Full = gsub("File skipped from exports: |File skipped from exports: media/", "", AA_Full),
+#     AA_Full = case_when(
+#       !is.na(AA_Full) ~ glue::glue("https://atrconsultingaf.surveycto.com/view/submission-attachment/{AA_Full}?uuid=uuid%3A{str_remove(KEY, 'uuid:')}"),
+#       TRUE ~ AA_Full
+#     ))
 
 # count(backcheck, phone_response_short)
 backcheck_complete <- filter(backcheck, phone_response_short == "Complete")
-
-
-
 
